555446395b32889d2de66c63cb408364d1849200
ninghao <ninghao@minxinjituan.cn>
2018-01-18 10:10:28 +0800

    springcloud demo

---
+++ micro-springcloud-demo/src/main/java/com/nh/micro/springcloud/demo/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.nh.micro.springcloud.demo;
+
+
+
+import org.springframework.context.annotation.Configuration; 
+import org.springframework.context.annotation.ImportResource; 
+
+@Configuration 
+@ImportResource("classpath:root-context.xml") 
+public class SpringConfig { }
